250b209317170a1bee2e51d0d0659cd811c22bf6
DaniilPodruchnyi <podruchnyyd@mail.ru>
2025-10-14 00:50:49 +0300

    Revert "Iter4"

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,29 +1,3 @@
 package main
 
-import (
-	"log"
-
-	"github.com/DaniilPodruchnyi/metrics-collector/internal/config"
-	"github.com/DaniilPodruchnyi/metrics-collector/internal/server"
-)
-
-func main() {
-	// Парсим конфигурацию
-	cfg, err := config.ParseServerConfig()
-	if err != nil {
-		log.Fatalf("Failed to parse configuration: %v", err)
-	}
-
-	// Логируем конфигурацию
-	cfg.LogConfig()
-
-	// Инициализация сервера
-	srv := server.New(cfg.Address)
-
-	log.Printf("Server starting on %s", cfg.Address)
-
-	// Запуск сервера
-	if err := srv.Start(); err != nil {
-		log.Fatal("Server failed:", err)
-	}
-}
+func main() {}
